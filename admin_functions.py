a68eaf87a7032c5b9d135e9d4d0126b4b3727b9a
YoeCris <yoelcriscatacora@gmail.com>
2024-07-02 16:14:53 -0500

    no se que hice pero ya

--- admin_functions.py
+++ admin_functions.py
@@ -63,62 +63,57 @@ def admin_interface():
                 add_case_button = st.form_submit_button("Agregar Documento")
         
                 if add_case_button:
-                    st.session_state.confirmation_action = {
-                        "type": "add",
-                        "code": code,
-                        "investigated_last_name": investigated_last_name,
-                        "investigated_first_name": investigated_first_name,
-                        "dni": dni,
-                        "reviewer": reviewer,
-                        "stage": stage
-                    }
-                    st.session_state.confirmation_message = f"¿Estás seguro de agregar el documento {code}?"
-                    st.experimental_rerun()
+                    if dni:
+                        user_management.create_case(code, investigated_last_name, investigated_first_name, dni, reviewer, stage)
+                        st.success(f"Documento {code} agregado exitosamente")
 
         elif case_action == "Editar Documento":
             st.subheader("Editar Documento")
-        
-            search_criterion = st.selectbox("Buscar por", ["Encargado", "DNI"])
+
+            if 'search_criterion' not in st.session_state:
+                st.session_state.search_criterion = "Encargado"
+                st.session_state.search_value = ""
+
+            search_criterion = st.selectbox("Buscar por", ["Encargado", "DNI"], index=["Encargado", "DNI"].index(st.session_state.search_criterion))
             if search_criterion == "Encargado":
                 users = user_management.get_users()
                 reviewers = [user['username'] for user in users if user['role'] == 'usuario']
-                selected_reviewer = st.selectbox("Seleccione el Encargado", reviewers)
+                selected_reviewer = st.selectbox("Seleccione el Encargado", reviewers, key="reviewer_selector")
                 find_case_button = st.button("Buscar casos por Encargado")
 
                 if find_case_button:
-                    st.session_state.search_criterion = "reviewer"
+                    st.session_state.search_criterion = "Encargado"
                     st.session_state.search_value = selected_reviewer
                     st.experimental_rerun()
 
             elif search_criterion == "DNI":
-                search_value = st.text_input("Ingrese DNI")
+                search_value = st.text_input("Ingrese DNI", key="dni_input")
                 find_case_button = st.button("Buscar casos por DNI")
 
                 if find_case_button:
-                    st.session_state.search_criterion = "dni"
+                    st.session_state.search_criterion = "DNI"
                     st.session_state.search_value = search_value
                     st.experimental_rerun()
 
-            if 'search_criterion' in st.session_state:
+            if st.session_state.search_value:
                 criterion = st.session_state.search_criterion
                 value = st.session_state.search_value
 
-                if criterion and value:
-                    if criterion == "reviewer":
-                        cases = user_management.get_cases_by_reviewer(value)
-                    else:
-                        cases = user_management.get_cases_by_dni(value)
+                if criterion == "Encargado":
+                    cases = user_management.get_cases_by_reviewer(value)
+                else:
+                    cases = user_management.get_cases_by_dni(value)
 
-                    if cases:
-                        df = pd.DataFrame(cases)
-                        df.columns = ['id', 'code', 'investigated_last_name', 'investigated_first_name', 'dni', 'reviewer', 'stage']
-                        st.dataframe(df)
-                        selected_case_id = st.selectbox("Seleccione el Id del caso a editar", df['id'])
-                        
-                        if st.button("Cargar Caso"):
-                            case = user_management.get_case(selected_case_id)
-                            st.session_state.edit_case = case
-                            st.experimental_rerun()
+                if cases:
+                    df = pd.DataFrame(cases)
+                    df.columns = ['id', 'code', 'investigated_last_name', 'investigated_first_name', 'dni', 'reviewer', 'stage']
+                    st.dataframe(df)
+                    selected_case_id = st.selectbox("Seleccione el Id del caso a editar", df['id'])
+
+                    if st.button("Cargar Caso"):
+                        case = user_management.get_case(selected_case_id)
+                        st.session_state.edit_case = case
+                        st.experimental_rerun()
 
             if 'edit_case' in st.session_state:
                 case = st.session_state.edit_case
@@ -127,79 +122,75 @@ def admin_interface():
                     investigated_last_name = st.text_input("Apellidos del Investigado", value=case.get('investigated_last_name', ''))
                     investigated_first_name = st.text_input("Nombre del Investigado", value=case.get('investigated_first_name', ''))
                     dni = st.text_input("DNI del Investigado", value=case.get('dni', ''), max_chars=8)
-            
+
                     if len(dni) != 8 or not dni.isdigit():
                         st.warning("DNI inválido")
                         dni = ""
-            
+
                     users = user_management.get_users()
                     normal_users = [user['username'] for user in users if user['role'] == 'usuario']
                     reviewer = st.selectbox("Encargado de Revisar el Documento", normal_users, index=normal_users.index(case.get('reviewer', '')))
 
                     stage = st.selectbox("Etapa del Caso", ['preparatoria', 'intermedia', 'juzgamiento'], index=['preparatoria', 'intermedia', 'juzgamiento'].index(case.get('stage', 'preparatoria')))
                     edit_case_button = st.form_submit_button("Actualizar Caso")
-            
+
                     if edit_case_button and dni:
-                        st.session_state.confirmation_action = {
-                            "type": "edit",
-                            "id": case['id'],
-                            "code": code,
-                            "investigated_last_name": investigated_last_name,
-                            "investigated_first_name": investigated_first_name,
-                            "dni": dni,
-                            "reviewer": reviewer,
-                            "stage": stage
-                        }
-                        st.session_state.confirmation_message = f"¿Estás seguro de actualizar el documento {code}?"
+                        user_management.update_case(case['id'], code, investigated_last_name, investigated_first_name, dni, reviewer, stage)
+                        st.success(f"Caso {code} actualizado exitosamente")
+                        st.session_state.pop('edit_case')
                         st.experimental_rerun()
 
         elif case_action == "Eliminar Documento":
             st.subheader("Eliminar Documento")
 
-            search_criterion = st.selectbox("Buscar por", ["Encargado", "DNI"])
+            if 'search_criterion' not in st.session_state:
+                st.session_state.search_criterion = "Encargado"
+                st.session_state.search_value = ""
+
+            search_criterion = st.selectbox("Buscar por", ["Encargado", "DNI"], index=["Encargado", "DNI"].index(st.session_state.search_criterion))
             if search_criterion == "Encargado":
                 users = user_management.get_users()
                 reviewers = [user['username'] for user in users if user['role'] == 'usuario']
-                selected_reviewer = st.selectbox("Seleccione el Encargado", reviewers)
+                selected_reviewer = st.selectbox("Seleccione el Encargado", reviewers, key="reviewer_selector")
                 find_case_button = st.button("Buscar casos por Encargado")
 
                 if find_case_button:
-                    st.session_state.search_criterion = "reviewer"
+                    st.session_state.search_criterion = "Encargado"
                     st.session_state.search_value = selected_reviewer
                     st.experimental_rerun()
 
             elif search_criterion == "DNI":
-                search_value = st.text_input("Ingrese DNI")
+                search_value = st.text_input("Ingrese DNI", key="dni_input")
                 find_case_button = st.button("Buscar casos por DNI")
 
                 if find_case_button:
-                    st.session_state.search_criterion = "dni"
+                    st.session_state.search_criterion = "DNI"
                     st.session_state.search_value = search_value
                     st.experimental_rerun()
 
-            if 'search_criterion' in st.session_state:
+            if st.session_state.search_value:
                 criterion = st.session_state.search_criterion
                 value = st.session_state.search_value
 
-                if criterion and value:
-                    if criterion == "reviewer":
-                        cases = user_management.get_cases_by_reviewer(value)
-                    else:
-                        cases = user_management.get_cases_by_dni(value)
+                if criterion == "Encargado":
+                    cases = user_management.get_cases_by_reviewer(value)
+                else:
+                    cases = user_management.get_cases_by_dni(value)
 
-                    if cases:
-                        df = pd.DataFrame(cases)
-                        df.columns = ['id', 'code', 'investigated_last_name', 'investigated_first_name', 'dni', 'reviewer', 'stage']
-                        st.dataframe(df)
-                        selected_case_id = st.selectbox("Seleccione el Id del caso a eliminar", df['id'])
-                        
-                        if st.button("Eliminar Caso"):
-                            st.session_state.confirmation_action = {
-                                "type": "delete",
-                                "id": selected_case_id
-                            }
-                            st.session_state.confirmation_message = f"¿Estás seguro de eliminar el caso con ID {selected_case_id}?"
+                if cases:
+                    df = pd.DataFrame(cases)
+                    df.columns = ['id', 'code', 'investigated_last_name', 'investigated_first_name', 'dni', 'reviewer', 'stage']
+                    st.dataframe(df)
+                    selected_case_id = st.selectbox("Seleccione el Id del caso a eliminar", df['id'])
+
+                    if st.button("Eliminar Caso"):
+                        confirmation = st.checkbox(f"Confirmar eliminación del caso con ID {selected_case_id}")
+                        if confirmation:
+                            user_management.delete_case(selected_case_id)
+                            st.success(f"Caso con ID {selected_case_id} eliminado exitosamente")
                             st.experimental_rerun()
+                        else:
+                            st.warning("Eliminación cancelada")
 
     elif choice == "Administrar Usuarios":
         st.subheader("Administrar Usuarios")
@@ -218,16 +209,8 @@ def admin_interface():
 
                 if add_user_button:
                     if len(dni) == 8 and dni.isdigit():
-                        st.session_state.confirmation_action = {
-                            "type": "add_user",
-                            "username": username,
-                            "password": password,
-                            "role": role,
-                            "first_name": first_name,
-                            "last_name": last_name,
-                            "dni": dni
-                        }
-                        st.session_state.confirmation_message = f"¿Estás seguro de agregar al usuario {username}?"
+                        user_management.create_user(username, password, role, first_name, last_name, dni)
+                        st.success(f"Usuario {username} agregado exitosamente")
                         st.experimental_rerun()
                     else:
                         st.warning("Por favor, ingrese un DNI válido de 8 dígitos.")
@@ -273,16 +256,16 @@ def admin_interface():
                     submit_button = st.form_submit_button("Actualizar Usuario")
             
                     if submit_button:
-                        st.session_state.confirmation_action = {
-                            "type": "edit_user",
-                            "username": user['username'],
-                            "password": new_password,
-                            "role": new_role,
-                            "first_name": new_first_name,
-                            "last_name": new_last_name,
-                            "dni": new_dni
-                        }
-                        st.session_state.confirmation_message = f"¿Estás seguro de actualizar al usuario {user['username']}?"
+                        user_management.update_user(
+                            username=user['username'],
+                            password=new_password,
+                            role=new_role,
+                            first_name=new_first_name,
+                            last_name=new_last_name,
+                            dni=new_dni
+                        )
+                        st.success("Usuario actualizado correctamente")
+                        del st.session_state.edit_user
                         st.experimental_rerun()
 
         elif user_action == "Eliminar Usuario":
@@ -294,45 +277,8 @@ def admin_interface():
                 if delete_user_button:
                     user = user_management.get_user_by_username(username)
                     if user:
-                        st.session_state.confirmation_action = {
-                            "type": "delete_user",
-                            "username": username
-                        }
-                        st.session_state.confirmation_message = f"¿Estás seguro de eliminar al usuario {username}?"
+                        user_management.delete_user(username)
+                        st.success(f"Usuario {username} eliminado exitosamente")
                         st.experimental_rerun()
                     else:
                         st.warning(f"No se encontró un usuario con nombre {username}")
-
-    if 'confirmation_message' in st.session_state:
-        st.warning(st.session_state.confirmation_message)
-        accept_button = st.button("Aceptar")
-        cancel_button = st.button("Cancelar")
-
-        if accept_button:
-            action = st.session_state.confirmation_action
-            if action['type'] == "add":
-                user_management.create_case(action['code'], action['investigated_last_name'], action['investigated_first_name'], action['dni'], action['reviewer'], action['stage'])
-                st.success(f"Documento {action['code']} agregado exitosamente")
-            elif action['type'] == "edit":
-                user_management.update_case(action['code'], action['code'], action['investigated_last_name'], action['investigated_first_name'], action['dni'], action['reviewer'], action['stage'])
-                st.success(f"Caso {action['code']} actualizado exitosamente")
-            elif action['type'] == "delete":
-                user_management.delete_case(action['code'])
-                st.success(f"Caso {action['code']} eliminado exitosamente")
-            elif action['type'] == "add_user":
-                user_management.create_user(action['username'], action['password'], action['role'], action['first_name'], action['last_name'], action['dni'])
-                st.success(f"Usuario {action['username']} agregado exitosamente")
-            elif action['type'] == "edit_user":
-                user_management.update_user(action['username'], action['password'], action['role'], action['first_name'], action['last_name'], action['dni'])
-                st.success(f"Usuario {action['username']} actualizado correctamente")
-            elif action['type'] == "delete_user":
-                user_management.delete_user(action['username'])
-                st.success(f"Usuario {action['username']} eliminado exitosamente")
-            st.session_state.pop('confirmation_message')
-            st.session_state.pop('confirmation_action')
-            st.experimental_rerun()
-
-        if cancel_button:
-            st.session_state.pop('confirmation_message')
-            st.session_state.pop('confirmation_action')
-            st.experimental_rerun()
